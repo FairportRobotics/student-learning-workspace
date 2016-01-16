6c71603d50a794d7d3145cdc7504c65ba5033728
RiseOfTheAlpacalypse <riseofthealpacalypse@gmail.com>
2016-01-16 13:42:36 -0500

    lol

---
+++ RobotKeepingState/src/edu/fairport/robotics/implementors/Kale.java
@@ -0,0 +1,39 @@
+package edu.fairport.robotics.implementors;
+
+import edu.fairport.robotics.interfaces.AbstractRobot;
+import edu.fairport.robotics.interfaces.GiveMeLightSensor;
+import edu.fairport.robotics.interfaces.GiveMeProximitySensor;
+import edu.fairport.robotics.interfaces.LightSensorIF;
+import edu.fairport.robotics.interfaces.ProximitySensorIF;
+import edu.fairport.robotics.master.Master;
+
+public class Kale extends AbstractRobot implements GiveMeLightSensor,
+		GiveMeProximitySensor {
+	double distance = 0;
+	double intensity = 0;
+
+	@Override
+	public void giveMeProximitySensor(ProximitySensorIF proximitySensor) {
+		// TODO Auto-generated method stub
+		distance = proximitySensor.getDistanceInMeters();
+		
+	}
+
+	@Override
+	public void giveMeLightSensor(LightSensorIF lightSensor) {
+		// TODO Auto-generated method stub
+		intensity = lightSensor.getIntensity();
+	}
+ 
+	@Override
+	public void tick() {
+		if (intensity > Master.MAX_INTENSITY && distance < Master.MIN_DISTANCE)
+			
+		{
+	this.doTheThing();		
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
