5f6171e01269da70798b637798754e32a0591aa0
Kevin <h1d3m3@yahoo.com>
2016-01-18 17:25:14 -0500

    Initial Version of RoboCodeBase

---
+++ RoboCodeBasic/src/edu/fairport/robocode/robots/samples/HelloWorldRobot.java
@@ -0,0 +1,18 @@
+package edu.fairport.robocode.robots.samples;
+
+import robocode.*;
+
+public class HelloWorldRobot extends Robot {
+    public void run() {
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+ 
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+}
