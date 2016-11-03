838c2b60973c90896ab73d585a8d430e6c6b7c7a
DTMSoundMaster <dtmsoundcorps@gmail.com>
2016-11-03 19:53:06 -0400

    Hi

---
+++ RoboCodeBasic/src/edu/fairport/robocode/robots/samples/DanielsBot.java
@@ -0,0 +1,18 @@
+package edu.fairport.robocode.robots.samples;
+
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
+public class DanielsBot extends AdvancedRobot {
+	
+	public void run() {
+		
+		while (true) {
+			
+			setAhead(100);
+			
+		}
+		
+	}
+
+}
