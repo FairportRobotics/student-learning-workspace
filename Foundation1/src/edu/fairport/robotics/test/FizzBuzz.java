d94452099da2f96e77024b7a365e72b29a1bb0b5
Kevin <h1d3m3@yahoo.com>
2017-02-06 19:21:30 -0500

    first

--- Foundation1/src/edu/fairport/robotics/test/FizzBuzz.java
+++ Foundation1/src/edu/fairport/robotics/test/FizzBuzz.java
@@ -3,18 +3,39 @@ package edu.fairport.robotics.test;
 public class FizzBuzz {
 	
 	public static boolean isFizz(int i) {
-		boolean result = false;
-		
+		boolean ran = false;
 		String iString = Integer.toString(i);
 		boolean hasFive = iString.contains("5");
 		
 		if (i % 5 == 0) {
-			result = true;
-		} else if (hasFive) {
-			result = true;
+			ran = true;
+			System.err.println("Fizz! ");
+		} 
+		
+		if (hasFive) {
+			ran = true;
+			System.err.println("Fizz! ");
 		}
 		
-		return result;
+		return ran;
+	}
+	
+	public static boolean isBuzz(int i) {
+	
+		boolean ran = false;
+		String iString = Integer.toString(i);
+		boolean hasSeven = iString.contains("7");
+		
+		if (i % 7 == 0) {
+			ran = true;
+			System.err.println("Buzz! ");
+		}
+		
+		if (hasSeven) {
+			ran = true;
+			System.err.println("Buzz! ");
+		}
+		return ran;
 	}
 
 	public static void main(String[] args) {
@@ -23,10 +44,8 @@ public class FizzBuzz {
 		
 		for (int i = 1; i <= 99; i++) {
 
-			if (FizzBuzz.isFizz(i)) {
-				System.err.println("Fizz!");
-			} else {
-				System.err.println(i);
+			if (!FizzBuzz.isFizz(i) && !FizzBuzz.isBuzz(i)) {
+				System.err.println();	
 			}
 		}
 	}
