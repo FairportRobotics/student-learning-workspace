ffd1dfdacc0e7e0895e2ce814c7fbdaef3a51a89
Kevin <h1d3m3@yahoo.com>
2017-02-06 19:06:13 -0500

    first

--- Foundation1/src/edu/fairport/robotics/test/FizzBuzz.java
+++ Foundation1/src/edu/fairport/robotics/test/FizzBuzz.java
@@ -1,14 +1,33 @@
 package edu.fairport.robotics.test;
 
 public class FizzBuzz {
+	
+	public static boolean isFizz(int i) {
+		boolean result = false;
+		
+		String iString = Integer.toString(i);
+		boolean hasFive = iString.contains("5");
+		
+		if (i % 5 == 0) {
+			result = true;
+		} else if (hasFive) {
+			result = true;
+		}
+		
+		return result;
+	}
 
 	public static void main(String[] args) {
+
+	
 		
 		for (int i = 1; i <= 99; i++) {
-			
-			
-			
-			System.err.println(i);
+
+			if (FizzBuzz.isFizz(i)) {
+				System.err.println("Fizz!");
+			} else {
+				System.err.println(i);
+			}
 		}
 	}
 }
