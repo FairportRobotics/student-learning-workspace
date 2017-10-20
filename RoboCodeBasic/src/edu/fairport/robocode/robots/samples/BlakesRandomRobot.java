86a98a7a1eec8d43feb60455395269be330d636f
frc <frc@frc-devstation>
2017-10-19 20:17:48 -0400

    This is the worst pile of junk you've ever seen

---
+++ RoboCodeBasic/src/edu/fairport/robocode/robots/samples/BlakesRandomRobot.java
@@ -0,0 +1,26 @@
+package edu.fairport.robocode.robots.samples;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class BlakesRandomRobot extends Robot{
+
+	@Override
+	public void run(){
+		while (true) {
+			ahead(60);
+			turnRight(60);
+            turnGunRight(360);
+            turnRight(60);
+            back(60);
+            turnRight(60);
+            turnGunRight(360);
+            turnRight(60);
+		}
+		
+	}
+	 public void onScannedRobot(ScannedRobotEvent e){
+	        fire(4);
+
+	 }
+}
